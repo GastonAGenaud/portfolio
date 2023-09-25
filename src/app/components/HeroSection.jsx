7e8b8157b08611e503ce7ccc275779f3059e5236
Gaston Genaud <gastongenaudar@gmail.com>
2023-09-24 22:20:09 -0300

    initial home page

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -10,7 +10,8 @@ const HeroSection = () => {
             <div className="grid grid-cols-1 sm:grid-cols-12">
                 <div className="col-span-7 place-self-center text-center sm:text-left">
                     <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold">
-                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600">Hello, I'm{" "}</span>
+
+                        <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-400 to-red-600">Hello, Im{" "}</span>
                         <br />
                         <TypeAnimation
                             sequence={[
