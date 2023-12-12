d5c3818c894bf9d57311deb3b8da94269df34cb6
Gaston Genaud <gastongenaudar@gmail.com>
2023-12-12 15:21:48 -0300

    added new project

--- src/lib/content/projects.ts
+++ src/lib/content/projects.ts
@@ -14,5 +14,14 @@ export const projectsSection: ProjectsSectionType = {
         year: 2023,
         tags: ['CSS Animations', 'Nextjs', 'TypeScript', 'Material-UI', 'TailwindCSS'],
     },
+    {
+        id: getId(),
+        name: 'Java Project with Selenium, TestNG, Maven, and Allure Reports',
+        url: 'https://github.com/GastonAGenaud/automation-selenium-java',
+        repo: 'https://github.com/GastonAGenaud/automation-selenium-java',
+        img: 'https://media.licdn.com/dms/image/D5612AQH4WDGQz3KWwg/article-cover_image-shrink_600_2000/0/1664433478843?e=2147483647&v=beta&t=rwXj2b_6BM3Lqh-y6DoquA_UL7q3aH1ND0FxDBeijN8',
+        year: 2023,
+        tags: ['Java', 'Selenium', 'TestNG', 'Maven', 'Allure Reports'],
+    },
     ],
 };
