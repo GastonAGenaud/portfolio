dc864fc224c53f1af37fc9cb3def031c7a7f194e
Gaston Genaud <gastongenaudar@gmail.com>
2023-12-12 14:44:16 -0300

    change resume word

--- src/lib/content/hero.ts
+++ src/lib/content/hero.ts
@@ -9,7 +9,7 @@ export const heroSection: HeroSectionType = {
     "As a seasoned Senior Software Developer Engineer in Test and DevOps Engineer, I specialize in creating robust, scalable, and efficient software solutions. My expertise lies in AWS for reliable cloud infrastructure, along with JavaScript, TypeScript, and Python for automation and backend development. With a strong focus on quality and innovation, I deliver exceptional results in both automated testing and DevOps practices.",
     specialText: 'Eager to explore new opportunities and challenges in software development in test and DevOps.',
     cta: {
-        title: 'View My Portfolio',
+        title: 'View My Resume',
         url: `/${resumeFileName}`,
         hideInDesktop: false,
     },
